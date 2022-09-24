26b88ac07d47f4e135ac2e41a8548f3fa0b33ff7
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-24 14:43:31 -0500

    Update controller to accommodate new params format

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,11 @@
 class Api::V1::UsersController < ApplicationController 
   def create
-    binding.pry 
     new_user = User.create!(user_params)
-    test = user_json_response(new_user, 201)
-    binding.pry 
+    user_json_response(new_user, 201)
   end
 
   private 
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
+      params.permit(:email, :password, :password_confirmation)
     end
 end
