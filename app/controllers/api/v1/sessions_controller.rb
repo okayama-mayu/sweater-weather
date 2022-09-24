fb2f419d4112a0842ed6cebe0c22413c91d7892b
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-24 15:02:42 -0500

    Add feature to return email and api_key with Session create

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::SessionsController < ApplicationController
   def create 
-    user = User.find_by(email: params[:email])
-    binding.pry 
+    user = User.find_by!(email: params[:email])
+    if user&.authenticate(params[:password])
+      user_json_response(user)
+    else 
+      
+    end
   end
 end
--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe 'Session CRUD' do
   describe 'Session Create' do 
     it 'can create a Session and returns an email and api_key' do 
       session_params = ({
-        "email": "whatever@eample.com", 
+        "email": "whatever@example.com", 
         "password": "password" 
       })
 
@@ -27,10 +27,10 @@ RSpec.describe 'Session CRUD' do
       user = User.find_by!(email: session_params[:email])
 
       expect(response).to be_successful 
-      expect(response).to have_http_status(201)
+      expect(response).to have_http_status(200)
 
       expect(response.body).to include("whatever@example.com")
-      expect(response.body).to include(created_user.password_digest)
+      expect(response.body).to include(user.password_digest)
       expect(user.email).to eq "whatever@example.com"
     end
   end
