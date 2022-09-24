db98c2206bd99cc2ea42dde64da91412a2451215
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-23 20:46:49 -0500

    Add alias for api keys to cassettes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,8 +66,8 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
-  config.filter_sensitive_data('Hide_Yo_Keys') { ENV['yelp_api_key'] }
-  config.filter_sensitive_data('Hide_Yo_Kids') { ENV['last_fm_key'] }
+  config.filter_sensitive_data('mapquest_key') { ENV['mapquest_key'] }
+  config.filter_sensitive_data('weather_key') { ENV['weather_key'] }
   config.configure_rspec_metadata!
   # config.default_cassette_options = {
   #   :match_requests_on => [:method,
