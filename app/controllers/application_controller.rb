79e9ead31033a84ac7898147f5c34c83b6a62e47
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-24 15:55:43 -0500

    Comment out lines we don't need to increase test coverage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   include Response, ExceptionHandler
   # helper_method :current_user  # don't actually need this because you don't have views 
 
-  def current_user # don't actually need this because you don't have views 
-    User.find(session[:user_id]) if session[:user_id]
-  end
+  # def current_user # don't actually need this because you don't have views 
+  #   User.find(session[:user_id]) if session[:user_id]
+  # end
 end
