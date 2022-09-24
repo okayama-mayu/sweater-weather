7ce80131a341b30c976c48b5e6a3e2330b38de56
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-24 00:07:40 -0500

    Add WeatherSearchFacade#weather_search

--- app/facades/weather_search_facade.rb
+++ app/facades/weather_search_facade.rb
@@ -2,7 +2,7 @@ class WeatherSearchFacade
   def self.weather_search(location)
     location_data = LocationService.search_location(location)
     coordinates = Location.new(location_data)
-    weather_data = WeatherService.search_weather(coordinates.lat, coordinates.lng)
-    weather = Weather.new(weather_data)
+    weather_data = WeatherService.search_coordinates(coordinates.lat, coordinates.lng)
+    forecast = Forecast.new(weather_data)
   end
 end
--- spec/facades/weather_search_facade_spec.rb
+++ spec/facades/weather_search_facade_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper' 
 
 RSpec.describe 'WeatherSearchFacade' do 
-  it 'returns a Location object', :vcr do 
-    location = WeatherSearchFacade.weather_search('denver, co')
+  it 'returns a Forecast object', :vcr do 
+    forecast = WeatherSearchFacade.weather_search('denver, co')
 
-    expect(location).to be_a Location
+    expect(forecast).to be_a Forecast
   end
 end
