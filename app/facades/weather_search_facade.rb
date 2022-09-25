777ec92427e863fd21c3fab3d3015737d8b123c0
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-24 20:51:16 -0500

    Add test for LocationService#search_directions

--- app/facades/weather_search_facade.rb
+++ app/facades/weather_search_facade.rb
@@ -9,7 +9,8 @@ class WeatherSearchFacade
   def self.road_trip_search(origin, destination)
     weather_data = weather_search(destination)
     time_data = LocationService.search_directions(origin, destination)
+    time_lapsed = time_data[:route][:time]
 
-    Roadtrip.new(origin, destination, weather_data, time_data)
+    Roadtrip.new(origin, destination, weather_data, time_lapsed)
   end
 end
