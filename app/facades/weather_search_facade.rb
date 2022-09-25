49412b7f2e80fdd64743c2fc46b2dd4a0cc1c631
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-24 22:59:06 -0500

    Add Facade feature to create Roadtrip object

--- app/facades/weather_search_facade.rb
+++ app/facades/weather_search_facade.rb
@@ -7,10 +7,16 @@ class WeatherSearchFacade
   end
 
   def self.road_trip_search(origin, destination)
-    weather_data = weather_search(destination)
+    weather_data = weather_data_raw(destination)
     time_data = LocationService.search_directions(origin, destination)
     time_lapsed = time_data[:route][:time]
-
+    
     Roadtrip.new(origin, destination, weather_data, time_lapsed)
+  end
+
+  def self.weather_data_raw(location)
+    location_data = LocationService.search_location(location)
+    coordinates = Location.new(location_data)
+    weather_data = WeatherService.search_coordinates(coordinates.lat, coordinates.lng)
   end
 end
