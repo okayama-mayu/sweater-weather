e78f86eae3843e51402df1684b1fc76b5baf3618
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-26 11:33:14 -0500

    Edit url and routing to book-search

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
       resources :road_trip, only: [:create]
 
-      resources :books, only: [:create]
+      post '/book-search', to: 'books#create'
     end
   end
 end
