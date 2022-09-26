86ddfe261feefeb9a036ffd359b137adc49fbef0
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-26 11:29:41 -0500

    Add feature to create and return a Book object given location and quantity

--- spec/requests/api/v1/books_spec.rb
+++ spec/requests/api/v1/books_spec.rb
@@ -15,25 +15,25 @@ RSpec.describe 'Book CRUD' do
       expect(response).to be_successful
       expect(response).to have_http_status 200
 
-      books = JSON.parse(response.body, symbolize_names: true)[:data] 
+      books = JSON.parse(response.body, symbolize_names: true)[:data]
 
       expect(books).to have_key(:id)
-      expect(books[:id]).to eq 'null'
+      expect(books[:id]).to eq nil
 
       expect(books).to have_key(:type)
-      expect(books[:type]).to eq "books"
+      expect(books[:type]).to eq "book"
 
       expect(books[:attributes]).to have_key(:destination)
-      expect(books[:attributes]).to eq "Denver, CO"
+      expect(books[:attributes][:destination]).to eq "Denver, CO"
 
       expect(books[:attributes][:forecast][:summary]).to be_a String
       expect(books[:attributes][:forecast][:temperature]).to be_a String
 
-      expect(books[:attributes][:total_books_]).to be_an Integer
+      expect(books[:attributes][:total_books_found]).to be_an Integer
 
-      expect(books[:books].count).to eq 5 
+      expect(books[:attributes][:books].count).to eq 5 
 
-      books[:books].each do |book| 
+      books[:attributes][:books].each do |book| 
         expect(book).to have_key(:isbn)
         expect(book[:isbn]).to be_an Array 
         expect(book[:isbn][0]).to be_a String 
