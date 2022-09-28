17c0fbad6b2d610dcfe7e95055bce78d81ceb707
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-28 12:22:21 -0500

    Comment out User#api_key (not secure) and edit UserSerializer to include :token

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
   has_secure_password
   has_secure_token
 
-  def api_key
-    password_digest
-    # not secure; use has_secure_token for rails, SecureRandom (tool that creates unique key)
-    # send screenshot to Juliet!
-  end
+  # def api_key
+  #   password_digest
+  #   # not secure; use has_secure_token for rails, SecureRandom (tool that creates unique key)
+  #   # send screenshot to Juliet!
+  # end
 end
