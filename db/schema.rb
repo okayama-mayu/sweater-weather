5a96260206d0803cf22f364a6c189b790d78ec2f
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-28 12:16:09 -0500

    Remove token:string and add token:token to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_28_170353) do
+ActiveRecord::Schema.define(version: 2022_09_28_171538) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,8 @@ ActiveRecord::Schema.define(version: 2022_09_28_170353) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
-    t.string "role"
+    t.string "token"
+    t.index ["token"], name: "index_users_on_token", unique: true
   end
 
 end
