073dccc806cc37230e7787cfed099b44f5b7c4bb
Mayu Takeda <mayu.takeda.utd@gmail.com>
2022-09-28 12:31:04 -0500

    Comment out test for User#api_key replaced by token attribute

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe User, type: :model do
   it { should have_secure_password }
   it { should have_secure_token }
 
-  it 'returns the api_key of a User' do 
-    user = User.create!(email: "whatever@example.com", "password": "password", "password_confirmation": "password")
+  # it 'returns the api_key of a User' do 
+  #   user = User.create!(email: "whatever@example.com", "password": "password", "password_confirmation": "password")
 
-    expect(user.api_key).to eq user.password_digest
-  end
+  #   expect(user.api_key).to eq user.password_digest
+  # end
 end
